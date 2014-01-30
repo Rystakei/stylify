9e66b76206be4a4f138dba8a0026d51585bc8d3f
Krista Williams <rystakei@gmail.com>
2014-01-29 22:44:24 -0500

    Modified functions

--- app/assets/javascripts/changes.js
+++ app/assets/javascripts/changes.js
@@ -5,21 +5,21 @@ $('.options-group').addClass("immutable");
 $('label').addClass("immutable");
 $('input').addClass("immutable");
 $('select').addClass("immutable");
+$('.styling-options').css('background-color', 'pink');
 
 $( "#bg" ).change(function() {
 	$('body').not('.immutable').css('background-color', $('#bg').val());
-
 });
 
 $( "#font-family" ).change(function() {
-	$('body').not('.immutable').css('font-family', $('#font-family').val());
+	$('body').find('*').not('.immutable').css('font-family', $('#font-family').val());
 
 });
 
 $('#font-size').keyup(function() {
 
 	var size = $('#font-size').val();
-	$('body').not('.immutable').css('font-size', size + 'px');
+	$('body').find('*').not('.immutable').css('font-size', size + 'px');
 });
 
 
